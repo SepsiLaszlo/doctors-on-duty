a2389cad16367176a8ae5fea9b990c548041d9fc
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2024-08-07 17:07:55 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  Doctor.create!(name: Faker::Name.name, on_duty: true)
+end
